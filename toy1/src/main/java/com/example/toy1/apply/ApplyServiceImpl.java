e9401eb7a7dff00b87d747f1bf32aefa2f4c7d10
최가연 <cn01803@naver.com>
2023-11-15 22:35:38 +0900

    feat: 지원 기능 구현

---
+++ toy1/src/main/java/com/example/toy1/apply/ApplyServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.toy1.apply;
+
+import com.example.wanted.jobposting.JobPostingEntity;
+import com.example.wanted.jobposting.JobPostingRepository;
+import com.example.wanted.user.UserEntity;
+import com.example.wanted.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class ApplyServiceImpl implements ApplyService {
+	private final ApplyRepository applyRepository;
+	private final UserRepository userRepository;
+	private final JobPostingRepository jobPostingRepository;
+
+	@Override
+	public void apply(Long userId, Long jobPostingId) throws Exception {
+		UserEntity userEntity = userRepository.findById(userId).orElseThrow(()->new Exception("사용자 정보를 찾을 수 없습니다."));
+		JobPostingEntity jobPostingEntity = jobPostingRepository.findById(jobPostingId).orElseThrow(()->new Exception("채용 공고 정보를 찾을 수 없습니다."));
+
+		if(applyRepository.existsByUserEntity_IdAndJobPostingEntity_JobPostingId(userEntity.getId(), jobPostingEntity.getJobPostingId())) {
+			throw new Exception("이미 지원한 항목입니다.");
+		}
+
+		ApplyEntity applyEntity = ApplyEntity.builder()
+			.userEntity(userEntity)
+			.jobPostingEntity(jobPostingEntity)
+			.build();
+
+		applyRepository.save(applyEntity);
+	}
+}
